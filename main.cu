8db4dcb6534f814b06889411bbfc7f380e6b5e87
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-04-10 09:24:49 -0700

    Update main.cu

--- main.cu
+++ main.cu
@@ -1,10 +1,46 @@
+
+// Not sure how many of these are actually needed.
+// ************************************************
+#include <iomanip>
+#include <string>
+#include <memory>
+#include <fstream>
+#include <ctime>
+#include <stdio.h>
+#include <inttypes.h>
+#include <cstddef>
+// ************************************************
+
 #include "PlatletSystem.h"
-// #include "SpringForce.h"
+
+std::string generateOutputFileName(std::string inputFileName) {
+	time_t now;
+	const int MAX_DATE = 64;
+	char theDate[MAX_DATE];
+	theDate[0] = '\0';
+
+	now = time(nullptr);
+
+	if (now != -1) {
+		strftime(theDate, MAX_DATE, "_%Y.%m.%d_%H-%M-%S", gmtime(&now));
+		return inputFileName + theDate;
+	}
+	return "";
+}
+
+// hard coded values for now. Later will accept xml file for intial values.
+std::shared_ptr<PlatletSystem> createPlatletSystem() {
+
+}
 
 
 int main() {  
 
+    auto pltSystem = createPlatletSystem();
+
     PlatletSystem platletSystem;
+
+    auto outputFileName = generateOutputFileName("test");
 
     platletSystem.initializePltSystem(5,5);
 
