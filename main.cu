4965f9c01c58578b6b60e3b8b1cb347520415c40
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-04-10 11:26:20 -0700

    Update main.cu
    Added run() which calls everything needed to run the simulation.
    
    Added CreatePlatletSystem() which calls PlatletSystemBuilder methods to read values in from XML and create the final system on device.

--- main.cu
+++ main.cu
@@ -11,7 +11,10 @@
 #include <cstddef>
 // ************************************************
 
+#include "pugixml/include/pugixml.hpp"
+
 #include "PlatletSystem.h"
+#include "PlatletSystemBuilder.h"
 
 std::string generateOutputFileName(std::string inputFileName) {
 	time_t now;
@@ -28,8 +31,81 @@ std::string generateOutputFileName(std::string inputFileName) {
 	return "";
 }
 
-// hard coded values for now. Later will accept xml file for intial values.
-std::shared_ptr<PlatletSystem> createPlatletSystem() {
+
+std::shared_ptr<PlatletSystem> createPlatletSystem(const char* schemeFile, std::shared_ptr<PlatletSystemBuilder> pltBuilder) {
+    // INPUT:   xml file with inital data
+    //          pointer to PlatletSystemBuilder
+    // OUTPUT:  pointer to the PlatletSystem (on device) with values read in through PlatletSystemBuilder.
+
+
+    pugi::xml_document doc;
+	pugi::xml_parse_result parseResult = doc.load_file(schemeFile);
+
+	if (!parseResult) {
+		std::cout << "parse error in createPlatletSystem: " << parseResult.description() << std::endl;
+		return nullptr;
+	}
+	pugi::xml_node root = doc.child("data");
+	pugi::xml_node nodes = root.child("nodes");
+	pugi::xml_node links = root.child("links");
+	pugi::xml_node props = root.child("settings");
+
+    // Check that no crucial data is missing.
+    // Default parameter values are stored in GeneralParams, so those are optional.
+	if (!(root && nodes && links)) {
+		std::cout << "Unable to find nessesary data.\n";
+    }
+    
+    // *****************************************************
+    // Load properties from the "settings" section.
+    // Check for parameters that match with GeneralParams (?)
+
+    if (auto p = props.child("absolute-temperature"))
+        pltBuilder->defaultTemperature = (p.text().as_double());
+        
+    // *****************************************************
+
+    // Add membrane nodes.
+
+
+    // Loop through node siblings and do pltBuilder->addMembraneNode
+
+
+    // *****************************************************
+
+    // Add membrane spring connections.
+
+
+    // Loop through link siblings and do pltBuilder->addMembraneEdge().
+
+
+    // *****************************************************
+	// Create and initialize (the pointer to) the final system on device().
+
+    auto pltModel = builder->Create_Platlet_System_On_Device();
+
+	std::cout << "model built" << "\n";
+
+	return pltModel;
+
+
+
+
+    
+}
+
+
+void run() {
+
+    double epsilon = 0.01;
+    double timeStep = 0.001;
+
+    // Inital creation of pointer to the PlatletSystemBuilder.
+    auto pltBuilder = std::make_shared<PlatletSystemBuilder>(epsilon, timeStep);
+
+    auto pltSystem = createPlatletSystem("testData.xml", pltBuilder);
+
+
 
 }
 
