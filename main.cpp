20b7aa5a19bb43a2e7d971c2939b378cb6f41a71
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-05-22 13:42:55 -0700

    Update main.cpp
    Moved parameters from GeneralParams to their respective classes.

--- main.cpp
+++ main.cpp
@@ -46,14 +46,17 @@ std::shared_ptr<PlatletSystem> createPlatletSystem(const char* schemeFile, std::
 		std::cout << "parse error in createPlatletSystem: " << parseResult.description() << std::endl;
 		return nullptr;
 	}
+
+    std::cout << "Setting pugixml variables\n";
 	pugi::xml_node root = doc.child("data");
-	pugi::xml_node nodes = root.child("nodes");
+	pugi::xml_node memNodes = root.child("membrane-nodes");
+    pugi::xml_node intNodes = root.child("interior-nodes");
 	pugi::xml_node links = root.child("links");
 	pugi::xml_node props = root.child("settings");
 
     // Check that no crucial data is missing.
     // Default parameter values are stored in GeneralParams, so those are optional.
-	if (!(root && nodes && links)) {
+	if (!(root && memNodes && links)) {
 		std::cout << "Unable to find nessesary data.\n";
     }
     
@@ -61,63 +64,102 @@ std::shared_ptr<PlatletSystem> createPlatletSystem(const char* schemeFile, std::
     // Load properties from the "settings" section.
     // Check for parameters that match with GeneralParams (?)
 
-    if (auto p = props.child("absoluteTemperature"))
+    std::cout << "Loading properties from xml\n";
+
+    if (auto p = props.child("temperature"))
         pltBuilder->generalParams.temperature = (p.text().as_double());
         
     if (auto p = props.child("viscousDamp"))
         pltBuilder->generalParams.viscousDamp = (p.text().as_double());
 
-    if (auto p = props.child("memSpringStiffness"))
-        pltBuilder->generalParams.memSpringStiffness = (p.text().as_double());
-    
-    if (auto p = props.child("memNodeMass"))
-        pltBuilder->generalParams.memNodeMass = (p.text().as_double());
-
     if (auto p = props.child("kB"))
         pltBuilder->generalParams.kB = (p.text().as_double());
+            
+    if (auto p = props.child("memSpringStiffness"))
+        pltBuilder->memSpringStiffness = (p.text().as_double());
+    
+    if (auto p = props.child("memNodeMass"))
+        pltBuilder->memNodeMass = (p.text().as_double());
+
+    if (auto p = props.child("intNodeMass"))
+        pltBuilder->intNodeMass = (p.text().as_double());
+
+    if (auto p = props.child("memNodeCount"))
+        pltBuilder->memNodeCount = (p.text().as_uint());
+
+    if (auto p = props.child("intNodeCount"))
+        pltBuilder->intNodeCount = (p.text().as_uint());
+
+
      
     // *****************************************************
 
+    std::cout << "Adding membrane nodes\n";
     // Add membrane nodes.
-
     double x, y, z; //variables to be used reading in data.
 
-	for (auto node = nodes.child("node"); node; node = node.next_sibling("node")) {
-		const char* text = node.text().as_string();
+    unsigned counter{ 0 };
 
+	for (auto memNode = memNodes.child("mem-node"); memNode; memNode = memNode.next_sibling("mem-node")) {
+		const char* text = memNode.text().as_string();
+        ++counter;
 		if (3 != sscanf(text, "%lf %lf %lf", &x, &y, &z)) {
-			std::cout << "parse node error\n";
+			std::cout << "parse memNode error\n";
 			return 0;
 		}
 		__attribute__ ((unused)) int unused = pltBuilder->addMembraneNode( glm::dvec3(x, y, z) );
     }
+
+    std::cout << "Counter: " << counter << '\n';
+
+
+
+    std::cout << "Adding interior nodes\n";
+    // Add interior nodes.
+    for (auto intNode = intNodes.child("int-node"); intNode; intNode = intNode.next_sibling("int-node")) {
+		const char* text = intNode.text().as_string();
+
+		if (3 != sscanf(text, "%lf %lf %lf", &x, &y, &z)) {
+			std::cout << "parse intNode error\n";
+			return 0;
+		}
+		__attribute__ ((unused)) int unused = pltBuilder->addInteriorNode( glm::dvec3(x, y, z) );
+    }
     
     // Check that the nodes are all there.
-    // pltBuilder->printNodes();
+    std::cout << "Printing nodes\n";
+    pltBuilder->printNodes();
 
     // *****************************************************
 
+
+    std::cout << "Adding membrane spring connections\n";
     // Add membrane spring connections.
 
-    double n1, n2; //variables to be used reading in data.
+    unsigned n1, n2; //variables to be used reading in data.
 
 	for (auto link = links.child("link"); link; link = link.next_sibling("link")) {
 		const char* text = link.text().as_string();
 
-		if (2 != sscanf(text, "%lf %lf", &n1, &n2)) {
-			std::cout << "parse node error\n";
+		if (2 != sscanf(text, "%u %u", &n1, &n2)) {
+			std::cout << "parse link error\n";
 			return 0;
 		}
+        std::cout << "n1: " << n1 << "\t n2: " << n2 << '\n';
 		__attribute__ ((unused)) int unused = pltBuilder->addMembraneEdge( n1, n2 );
     }
 
+    std::cout << "Setting fixed nodes\n";
+    // Read in fixed nodes.
     pugi::xml_node fixedRoot = root.child("fixed");
 	if (fixedRoot) {
-		for (auto node = fixedRoot.child("node"); node; node = node.next_sibling("node"))
+		for (auto node = fixedRoot.child("nodeID"); node; node = node.next_sibling("nodeID"))
 			pltBuilder->fixNode(node.text().as_uint());
 	}
 
-    // pltBuilder->printEdges();
+    std::cout << "Printing membrane spring edges\n";
+    pltBuilder->printEdges();
+
     // *****************************************************
 	// Create and initialize (the pointer to) the final system on device().
 
@@ -127,14 +169,7 @@ std::shared_ptr<PlatletSystem> createPlatletSystem(const char* schemeFile, std::
 
 	std::cout << "model built" << "\n";
 
-    return ptr_Platlet_System_Host;
-    
-    //return nullptr;
-
-
-
-
-    
+    return ptr_Platlet_System_Host;    
 }
 
 
@@ -148,10 +183,13 @@ void run() {
     double timeStep = 0.001;
 
     // Inital creation of pointer to the PlatletSystemBuilder.
+    std::cout << "Creating pltBuilder\n";
     auto pltBuilder = std::make_shared<PlatletSystemBuilder>(epsilon, timeStep);
 
+    std::cout << "Creating pltSystem\n";
     auto pltSystem = createPlatletSystem("info.xml", pltBuilder);
 
+    std::cout << "Generating output filename\n";
     auto outputFileName = generateOutputFileNameByDate("Test");
 
     std::cout << outputFileName << '\n';
@@ -177,6 +215,7 @@ void run() {
 
 int main(int argc, char** argv) {  
 
+    std::cout << "Running\n";
     run();
            
     return 0;
