b8eae8357e6d39a8c7c6c06b709ebdb74f50c58a
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-09-25 10:18:59 -0700

    Create IFunction.h
    Utilize polymorphism to call all major calculations.

---
+++ src/IFunction.h
@@ -0,0 +1,19 @@
+#ifndef IFUNCTION_H_
+#define IFUNCTION_H_
+
+// *******************************************
+/* 
+Used to call various functions that act on the system by calling functors on the device. Most calls to execute() will simply contain a for_each or transform that invokes a device functor.
+
+Example: each type of force is calculated by a different device functor, called from a single IFunction object.
+*/
+// *******************************************
+
+
+class IFunction {
+    public:
+    // virtual ~IFunction() {}
+    virtual void execute() = 0;
+};
+
+#endif
