adbde23dbff3b22e598c98661daee5dc02654fff
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-09-25 10:02:21 -0700

    Update PlatletSystem.
    The system now holds only the data and the controller that runs the system.

---
+++ src/PlatletSystem.h
@@ -0,0 +1,27 @@
+#ifndef PLATLET_SYSTEM_H_
+#define PLATLET_SYSTEM_H_
+
+
+
+class NodeData;
+class PlatletSystemController;
+
+
+class PlatletSystem {
+
+    private:
+
+    NodeData nodeData;
+
+    PlatletSystemController platletSystemController;
+
+
+    public:
+
+    void runSystem();
+
+
+};
+
+
+#endif
