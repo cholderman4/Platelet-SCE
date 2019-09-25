5754e5f1b573ec260b8b39daa60964524a01c285
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-09-25 11:45:49 -0700

    Clarified interaction between Parameter classes.
    IReadParameter finds or sends values from an (external) source.
    
    IParameterList manages that source internally, so it can do all that plus add, set, and change those values.

--- src/IParameterList.h
+++ src/IParameterList.h
@@ -1,6 +1,14 @@
 #ifndef I_PARAMETER_LIST_H_
 #define I_PARAMETER_LIST_H_
 
+
+// *******************************************
+/* 
+A specific case of an IReadParameter object (hence the inheritance) that internally manages the source of parameter key/values (e.g. a map of some kind). This means we can search/iterate through parameters as before, but additionally we can add, set, or otherwise change those values.
+*/
+// *******************************************
+
+
 #include <string>
 
 #include "IReadParameter.h"
@@ -9,12 +17,27 @@
 class IParameterList : public IReadParameter {
 
     public:
-    virtual void addParameter(std::string key, double value);
-    virtual void setValue(std::string key, double value);
+    // Possibly duplicate functionality.
+    // Maybe clients want different behavior if the value already exists or not.
+    // Possibly one or both of these could be changed to return bool.
+    virtual void addValue(
+        const std::string key, 
+        const double& value) = 0;
 
-    virtual bool findValue(const std::string key, double& value);
+    virtual void setValue(
+        const std::string key, 
+        const double& value) = 0;
+
+
+    // IReadParameter
+    virtual bool findValue(
+        const std::string key, 
+        double& value) = 0;
+
+    virtual void sendValuesToList(IParameterList& parameterList) = 0;
+
 
 
 };
 
-#endif // I_RETREIVE_PARAMETER_H_
+#endif // I_PARAMETER_LIST_H_
--- src/IReadParameter.h
+++ src/IReadParameter.h
@@ -1,6 +1,15 @@
 #ifndef I_READ_PARAMETER_H_
 #define I_READ_PARAMETER_H_
 
+// *******************************************
+/* 
+Used to read in parameter values, by their key, from some source (XML, command line, itself etc.). Clients can search for a specific key/vlaue, or receive all values into a parameterList. In either case, this class knows how to search/iterate through its source and is responsible to do so (or possibly just return false).
+
+Note that a IParameterList has two options to search through values from this: either it can iterate thorugh its own list and call findValue() each time, or allow this to iterate though its source and setValues()/addValues() for each entry.
+*/
+// *******************************************
+
+
 #include <string>
 
 class IParameterList;
@@ -9,9 +18,15 @@ class IParameterList;
 class IReadParameter {
 
     public:
-    virtual bool findValue(const std::string key, double& value);
-    virtual void sendValuesToList(IParameterList& parameterList);
+    // Find (and get) a specific value.
+    // This may not be supported and implemented with empty function (returning false).
+    virtual bool findValue(
+                    const std::string key, 
+                    double& value) = 0;
+    
+    // Iterate through source values, calling parameterList.setValue().
+    virtual void sendValuesToList(IParameterList& parameterList) = 0;
 
 };
 
-#endif // I_RETREIVE_PARAMETER_H_
+#endif // I_PARAMETER_LIST_H_
